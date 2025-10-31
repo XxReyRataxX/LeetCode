1393226cc18ca1d8aa53ec2a997b5cadb2859f8c
Antonio Marí González <133799208+XxReyRataxX@users.noreply.github.com>
2025-10-31 10:53:55 +0100

    Time: 0 ms (100%), Space: 12.5 MB (64.49%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -0,0 +1,16 @@
+# @lc app=leetcode id=151 lang=python
+#
+# [151] Reverse Words in a String
+#
+
+# @lc code=start
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        
+        listac = s.split()
+        return ' '.join(reversed(listac))
+# @lc code=end

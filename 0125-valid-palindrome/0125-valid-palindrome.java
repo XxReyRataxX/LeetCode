0cf4435cb5356395aaee6bf74f8836a646faaf0c
Antonio Marí González <133799208+XxReyRataxX@users.noreply.github.com>
2026-03-30 21:06:34 +0200

    Time: 14 ms (31.97%), Space: 46.5 MB (21.85%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=125 lang=java
+ *
+ * [125] Valid Palindrome
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
+// @lc code=end
+
